8f745d0754a5e67d48a3d52e4081bb8959a0b973
nikulet <72616263+nikulet@users.noreply.github.com>
2024-05-22 11:03:21 +0200

    Delete tarea1n2 directory

--- tarea1n2/direccion/Direccion.java
+++
@@ -1,5 +0,0 @@
-package tarea1n2.direccion;
-
-public interface Direccion {
-    String getDireccionCompleta();
-}
--- tarea1n2/numero_telefono/NumeroTelefono.java
+++
@@ -1,5 +0,0 @@
-package tarea1n2.numero_telefono;
-
-public interface NumeroTelefono {
-    String getNumeroTelefono();
-}
